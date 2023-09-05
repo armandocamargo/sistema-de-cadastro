12e03dae63d6fd39f21c50126ccc4a0f529ae52b
Armando Camargo <armando_margo@hotmail.com>
2023-09-05 20:15:09 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação esta se conectando ao banco de dados")
 // mais um console log de teste: 
 console.log("o paulo fez esse commit sem voce ver na aula")
+console.log("enviando para o github")
